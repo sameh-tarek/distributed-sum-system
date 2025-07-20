7907a9f2d879394a14cee017a64f1c162cec18c6
Sameh.Tarek <tarksame7@gmail.com>
2025-07-20 12:28:56 +0300

    feat(grpc): implement CalculatorService with Add RPC method

---
+++ calculator-service-gRPC/src/main/java/com/example/calculatorservicegrpc/CalculatorServiceGRpcApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculatorservicegrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorServiceGRpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorServiceGRpcApplication.class, args);
+    }
+
+}
---
+++ calculator-service-gRPC/src/main/java/com/example/calculatorservicegrpc/grpc/CalculatorServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.calculatorservicegrpc.grpc;
+
+import com.example.calculatorservicegrpc.AddRequest;
+import com.example.calculatorservicegrpc.AddResponse;
+import com.example.calculatorservicegrpc.CalculatorServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class CalculatorServiceImpl extends CalculatorServiceGrpc.CalculatorServiceImplBase {
+    @Override
+    public void add(AddRequest request, StreamObserver<AddResponse> responseObserver) {
+        int sum = request.getNum1() + request.getNum2();
+
+        AddResponse response = AddResponse.newBuilder()
+                .setSum(sum)
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
