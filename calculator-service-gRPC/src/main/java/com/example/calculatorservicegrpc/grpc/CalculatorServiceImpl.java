ef7697ea63c6e21d45dac8467818bf24f668cd35
Sameh.Tarek <tarksame7@gmail.com>
2025-07-20 18:04:53 +0300

    feat(kafka): Add Kafka UI (Kafdrop) via Docker and kafka producer in gRPC service

--- calculator-service-gRPC/src/main/java/com/example/calculatorservicegrpc/grpc/CalculatorServiceImpl.java
+++ calculator-service-gRPC/src/main/java/com/example/calculatorservicegrpc/grpc/CalculatorServiceImpl.java
@@ -3,14 +3,21 @@ package com.example.calculatorservicegrpc.grpc;
 import com.example.calculatorservicegrpc.AddRequest;
 import com.example.calculatorservicegrpc.AddResponse;
 import com.example.calculatorservicegrpc.CalculatorServiceGrpc;
+import com.example.calculatorservicegrpc.kafka.KafkaSumProducer;
 import io.grpc.stub.StreamObserver;
+import lombok.RequiredArgsConstructor;
 import net.devh.boot.grpc.server.service.GrpcService;
 
+@RequiredArgsConstructor
 @GrpcService
 public class CalculatorServiceImpl extends CalculatorServiceGrpc.CalculatorServiceImplBase {
+    private final KafkaSumProducer kafkaSumProducer;
+
     @Override
     public void add(AddRequest request, StreamObserver<AddResponse> responseObserver) {
         int sum = request.getNum1() + request.getNum2();
+
+        kafkaSumProducer.sendSum(sum);
 
         AddResponse response = AddResponse.newBuilder()
                 .setSum(sum)
